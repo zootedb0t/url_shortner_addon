a0b20d58702d92a9ca170235de7087491cfc0ede
zootedb0t <05kraken@proton.me>
2023-03-02 19:52:53 +0530

    fix code scanning warnings

--- option/options.js
+++ option/options.js
@@ -1,44 +1,44 @@
-const apiKey = document.querySelector("#apikey");
-const groupId = document.querySelector("#groupid");
-const submitKey = document.querySelector("#addkey");
+const apiKey = document.querySelector('#apikey')
+const groupId = document.querySelector('#groupid')
+const submitKey = document.querySelector('#addkey')
 
-submitKey.addEventListener("click", () => {
-  if (apiKey.value === "" || groupId.value === "") {
-    alert("Enter a valid api key");
+submitKey.addEventListener('click', () => {
+  if (apiKey.value === '' || groupId.value === '') {
+    alert('Enter a valid api key')
   } else {
     browser.storage.local.set({
       authCredentials: {
         authorization: apiKey.value,
-        groupid: groupId.value,
-      },
-    });
-    alert("API key added!!");
+        groupid: groupId.value
+      }
+    })
+    alert('API key added!!')
 
     // Set api-key to local storage
     browser.storage.local
       .get()
       .then((result) => {
-        localStorage.setItem("authKey", result.authCredentials.authorization);
-        localStorage.setItem("groupKey", result.authCredentials.groupid);
+        localStorage.setItem('authKey', result.authCredentials.authorization)
+        localStorage.setItem('groupKey', result.authCredentials.groupid)
       })
       .catch(() => {
-        alert("Please add api key!!\n");
-      });
+        alert('Please add api key!!\n')
+      })
   }
-});
+})
 
 function onError(e) {
-  console.error(e);
+  console.error(e)
 }
 
 function updateUI(restoredSettings) {
-  apiKey.value = restoredSettings.authCredentials.authorization || "";
-  groupId.value = restoredSettings.authCredentials.groupid || "";
-  const para = document.createElement("p");
-  para.id = `keypresent`;
-  para.innerText = "Api key already present";
-  document.body.appendChild(para);
+  apiKey.value = restoredSettings.authCredentials.authorization || ''
+  groupId.value = restoredSettings.authCredentials.groupid || ''
+  const para = document.createElement('p')
+  para.id = 'keypresent'
+  para.innerText = 'Api key already present'
+  document.body.appendChild(para)
 }
 
-const gettingApiKeys = browser.storage.local.get();
-gettingApiKeys.then(updateUI, onError);
+const gettingApiKeys = browser.storage.local.get()
+gettingApiKeys.then(updateUI, onError)
--- scripts.js
+++ scripts.js
@@ -1,64 +1,64 @@
-const showUrl = document.getElementById("copy-url");
-const longUrl = document.getElementById("longurl");
-const form = document.getElementById("formsubmit");
-const openSetting = document.getElementById("setting-img");
+const showUrl = document.getElementById('copy-url')
+const longUrl = document.getElementById('longurl')
+const form = document.getElementById('formsubmit')
+const openSetting = document.getElementById('setting-img')
 
 // Put current tab url into input field
-const url = document.querySelector("#geturl");
-url.addEventListener("click", () => {
+const url = document.querySelector('#geturl')
+url.addEventListener('click', () => {
   browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
-    let url = tabs[0].url;
-    longUrl.value = url;
-  });
-});
+    const url = tabs[0].url
+    longUrl.value = url
+  })
+})
 
 // Open options pages
-openSetting.addEventListener("click", () => {
-  browser.runtime.openOptionsPage();
-});
+openSetting.addEventListener('click', () => {
+  browser.runtime.openOptionsPage()
+})
 
 // await used inside async function make program wait until promise resolves
 // Send data to bitly server and fetch response
-form.addEventListener("click", async () => {
-  const ath = localStorage.getItem("authKey");
-  const grp = localStorage.getItem("groupKey");
-  const origninalUrl = longUrl.value;
+form.addEventListener('click', async () => {
+  const ath = localStorage.getItem('authKey')
+  const grp = localStorage.getItem('groupKey')
+  const origninalUrl = longUrl.value
 
-  showUrl.innerText = "LOADING...";
+  showUrl.innerText = 'LOADING...'
 
-  if (origninalUrl !== "") {
+  if (origninalUrl !== '') {
     try {
-      const bitlyUrl = await fetch("https://api-ssl.bitly.com/v4/shorten", {
-        method: "POST",
+      const bitlyUrl = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+        method: 'POST',
         headers: {
           Authorization: ath,
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           long_url: origninalUrl,
-          domain: "bit.ly",
-          group_guid: grp,
-        }),
-      });
-      const data = await bitlyUrl.json();
+          domain: 'bit.ly',
+          group_guid: grp
+        })
+      })
+      const data = await bitlyUrl.json()
       // Debugging
       // console.log(data);
-      showUrl.textContent = data.link;
+      showUrl.textContent = data.link
 
-      const elem = document.createElement("button");
-      elem.id = `copytoclipboard`;
-      elem.innerText = "Copy to clipboard";
-      elem.addEventListener("click", () => {
-        const cb = navigator.clipboard;
-        const data = document.querySelector("#copy-url");
-        cb.writeText(data.innerText);
-      });
-      document.body.appendChild(elem);
+      const elem = document.createElement('button')
+      elem.id = 'copytoclipboard'
+      elem.innerText = 'Copy to clipboard'
+      elem.addEventListener('click', () => {
+        const cb = navigator.clipboard
+        const data = document.querySelector('#copy-url')
+        cb.writeText(data.innerText)
+      })
+      document.body.appendChild(elem)
     } catch (error) {
-      console.log(error);
-      showUrl.innerText = "An error occured!";
+      console.log(error)
+      showUrl.innerText = 'An error occured!'
     }
   } else {
-    showUrl.innerText = "URL field cannot be empty";
+    showUrl.innerText = 'URL field cannot be empty'
   }
-});
+})
